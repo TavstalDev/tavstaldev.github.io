b824fe17a91c0c3f556875c56422360f38f0df83
tavstal <tavstal@gmail.com>
2025-07-16 22:01:15 +0200

    add .gitignore, eslint configuration, and Vite setup for React and Tailwind CSS

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    /*https: {
+      key: fs.readFileSync('./certs/dev.pem'),
+      cert: fs.readFileSync('./certs/dev.crt'),
+    },*/
+    host: 'localhost',  // Listening network
+    port: 8090,       // Listening port
+    strictPort: true  // Ensures Vite fails if the port is already in use
+  },
+  plugins: [tailwindcss(), react()],
+  build: {
+    sourcemap: false, // Disable source map generation
+  },
+})
