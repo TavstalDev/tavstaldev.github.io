407fb67e7a4bb439ecae444f9bb87260e8e6b63f
tavstal <tavstal@gmail.com>
2025-07-16 22:30:42 +0200

    update deploy script to use 'dist' directory and set base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   server: {
     /*https: {
       key: fs.readFileSync('./certs/dev.pem'),
